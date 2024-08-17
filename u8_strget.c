323c27f344070587eea91e197347308497190241
Pavel Sytník <pashasytnik@gmail.com>
2024-08-17 22:17:36 +0400

    Make u8_strget() return code point at given index of UTF-8 string

--- u8_strget.c
+++ u8_strget.c
@@ -1,8 +1,13 @@
 #include "utf.h"
 
-size_t u8_strget(const char *s, size_t i)
+#define CHAR_PTR_TO_UTF8 (utf8_t) (unsigned char)
+
+// Also unsafe code
+utf32_t u8_strget(const char *s, size_t i)
 {
     size_t shift = 0;
+    utf8_t ch;
+
     while (i-- > 0) {
         if ((s[shift] & 0x80) == 0x00) {
             shift += 1;
@@ -14,5 +19,24 @@ size_t u8_strget(const char *s, size_t i)
             shift += 4;
         }
     }
-    return shift;
+
+    if ((s[shift] & 0x80) == 0x00) {
+        ch = CHAR_PTR_TO_UTF8 s[shift];
+    } else if ((s[shift] & 0xE0) == 0xC0) {
+        ch = CHAR_PTR_TO_UTF8 s[shift + 0] << 8 |
+             CHAR_PTR_TO_UTF8 s[shift + 1];
+    } else if ((s[shift] & 0xF0) == 0xE0) {
+        ch = CHAR_PTR_TO_UTF8 s[shift + 0] << 16 |
+             CHAR_PTR_TO_UTF8 s[shift + 1] << 8  |
+             CHAR_PTR_TO_UTF8 s[shift + 2];
+    } else if ((s[shift] & 0xF8) == 0xF0) {
+        ch = CHAR_PTR_TO_UTF8 s[shift + 0] << 24 |
+             CHAR_PTR_TO_UTF8 s[shift + 1] << 16 |
+             CHAR_PTR_TO_UTF8 s[shift + 2] << 8  |
+             CHAR_PTR_TO_UTF8 s[shift + 3];
+    } else {
+        ch = 0xFFFFFFFF;
+    }
+
+    return u8tou32(ch);
 }
--- utf.h
+++ utf.h
@@ -8,7 +8,7 @@ typedef uint32_t utf8_t, utf16_t, utf32_t;
 
 utf8_t u32tou8(utf32_t c);
 utf32_t u8tou32(utf8_t c);
-size_t u8_strget(const char *s, size_t i);
+utf32_t u8_strget(const char *s, size_t i);
 size_t u8_strlen(const char *s);
 
 #endif /* UTF_H */
