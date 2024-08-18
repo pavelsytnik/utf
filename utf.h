0ff2fc81cc6b969fec9c1130fae5faa274791976
Pavel Sytník <pashasytnik@gmail.com>
2024-08-18 20:00:52 +0400

    Add conversion from UTF-32 character to UTF-16 one

--- utf.h
+++ utf.h
@@ -8,6 +8,7 @@ typedef uint32_t utf8_t, utf16_t, utf32_t;
 
 utf8_t u32tou8(utf32_t c);
 utf32_t u8tou32(utf8_t c);
+utf16_t u32tou16(utf32_t c);
 utf32_t u8_strget(const char *s, size_t i);
 size_t u8_strlen(const char *s);
 
