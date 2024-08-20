dab4f07431269b4f20a1c0c6e8f9d266699e103b
Pavel Sytník <pashasytnik@gmail.com>
2024-08-20 16:57:55 +0400

    Significantly optimize str_u8tou32()

--- str_u8tou32.c
+++ str_u8tou32.c
@@ -1,12 +1,25 @@
 #include "utf.h"
 
-// Don't know why I'm writing this again... TOTALLY UNSAFE!!!
 void str_u8tou32(char32_t *restrict dst, const char8_t *restrict src)
 {
-    size_t src_len = u8_strlen(src);
-    for (size_t i = 0; i < src_len; i++)
-    {
-        dst[i] = u8_strget(src, i);
+    while (*src != 0) {
+        char32_t ch = 0;
+        if ((*src & 0x80) == 0x00) {
+            ch |= *src++;
+        } else if ((*src & 0xE0) == 0xC0) {
+            ch |= (*src++ & 0x1F) << 6;
+            ch |=  *src++ & 0x3F;
+        } else if ((*src & 0xF0) == 0xE0) {
+            ch |= (*src++ & 0x0F) << 12;
+            ch |= (*src++ & 0x3F) << 6;
+            ch |=  *src++ & 0x3F;
+        } else if ((*src & 0xF8) == 0xF0) {
+            ch |= (*src++ & 0x07) << 18;
+            ch |= (*src++ & 0x3F) << 12;
+            ch |= (*src++ & 0x3F) << 6;
+            ch |=  *src++ & 0x3F;
+        }
+        *dst++ = ch;
     }
-    dst[src_len] = 0;
+    *dst = 0;
 }
