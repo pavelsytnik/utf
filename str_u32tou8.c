346dd4a43b871597c439979b7dfe2ffad68c2912
Pavel Sytník <pashasytnik@gmail.com>
2024-08-20 19:47:08 +0400

    Add conversion from UTF-32 string to UTF-8 one

---
+++ str_u32tou8.c
@@ -0,0 +1,24 @@
+#include "utf.h"
+
+void str_u32tou8(char8_t *restrict dst, const char32_t *restrict src)
+{
+    while (*src != 0) {
+        if (*src < 0x80) {
+            *dst++ = *src;
+        } else if (*src < 0x800) {
+            *dst++ = *src >> 6 | 0xC0;
+            *dst++ = *src & 0x3F | 0x80;
+        } else if (*src < 0x10000) {
+            *dst++ = *src >> 12 | 0xE0;
+            *dst++ = *src >> 6 & 0x3F | 0x80;
+            *dst++ = *src & 0x3F | 0x80;
+        } else if (*src < 0x110000) {
+            *dst++ = *src >> 18 | 0xF0;
+            *dst++ = *src >> 12 & 0x3F | 0x80;
+            *dst++ = *src >> 6 & 0x3F | 0x80;
+            *dst++ = *src & 0x3F | 0x80;
+        }
+        src++;
+    }
+    *dst = 0;
+}
