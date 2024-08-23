2ac98f54fec72380178c75165fa66cf0a8abf653
Pavel Sytník <pashasytnik@gmail.com>
2024-08-23 18:00:49 +0400

    Refactor utf8_checkchar.c

--- utf8_checkchar.c
+++ utf8_checkchar.c
@@ -4,9 +4,11 @@
 
 #include "utf.h"
 
-#define UTF8_TRY_INCREMENT_STRING_POINTER(str) do {\
-    enum utf_error err = utf_inc_strp(str);\
-    if (err) return err;\
+#define UTF8_TRY_INCREMENT_STRING_POINTER(strp) do { \
+    if (++*(strp) == 0) \
+        return UTF_TRUNCATED; /* NOT ENOUGH ROOM */ \
+    if ((**(strp) & 0xC0) != 0x80) \
+        return UTF_TRUNCATED; \
 } while (0)
 
 static int utf8_is_overlong(uint32_t codepoint, int length)
@@ -14,75 +16,6 @@ static int utf8_is_overlong(uint32_t codepoint, int length)
     return codepoint <= 0x7F   && length > 1 ||
            codepoint <= 0x7FF  && length > 2 ||
            codepoint <= 0xFFFF && length > 3  ;
-}
-
-static enum utf_error utf8_inc_strp(const char8_t **p_s)
-{
-    if (++*p_s == 0)
-        return UTF_TRUNCATED;
-
-    if ((**p_s & 0xC0) != 0x80)
-        return UTF_TRUNCATED;
-
-    return UTF_OK;
-}
-
-static enum utf_error utf8_getseq1(const char8_t **p_s, uint32_t *p_codepoint)
-{
-    if (**p_s == 0)
-        return UTF_TRUNCATED;
-
-    *p_codepoint = **p_s;
-
-    return UTF_OK;
-}
-
-static enum utf_error utf8_getseq2(const char8_t **p_s, uint32_t *p_codepoint)
-{
-    if (**p_s == 0)
-        return UTF_TRUNCATED;
-
-    *p_codepoint = (**p_s & 0x1F) << 6;
-
-    UTF8_TRY_INCREMENT_STRING_POINTER(p_s);
-    *p_codepoint |= **p_s & 0x3F;
-
-    return UTF_OK;
-}
-
-static enum utf_error utf8_getseq3(const char8_t **p_s, uint32_t *p_codepoint)
-{
-    if (**p_s == 0)
-        return UTF_TRUNCATED;
-
-    *p_codepoint = (**p_s & 0x0F) << 12;
-
-    UTF8_TRY_INCREMENT_STRING_POINTER(p_s);
-    *p_codepoint |= (**p_s & 0x3F) << 6;
-
-    UTF8_TRY_INCREMENT_STRING_POINTER(p_s);
-    *p_codepoint |= **p_s & 0x3F;
-
-    return UTF_OK;
-}
-
-static enum utf_error utf8_getseq4(const char8_t **p_s, uint32_t *p_codepoint)
-{
-    if (**p_s == 0)
-        return UTF_TRUNCATED;
-
-    *p_codepoint = (**p_s & 0x07) << 18;
-
-    UTF8_TRY_INCREMENT_STRING_POINTER(p_s);
-    *p_codepoint |= (**p_s & 0x3F) << 12;
-
-    UTF8_TRY_INCREMENT_STRING_POINTER(p_s);
-    *p_codepoint |= (**p_s & 0x3F) << 6;
-
-    UTF8_TRY_INCREMENT_STRING_POINTER(p_s);
-    *p_codepoint |= **p_s & 0x3F;
-
-    return UTF_OK;
 }
 
 static int utf8_seqlen(const char8_t *c)
@@ -99,33 +32,63 @@ static int utf8_seqlen(const char8_t *c)
         return 0;
 }
 
+static enum utf_error utf8_getseq(const char8_t **p_s,
+                                  uint32_t *p_codepoint,
+                                  int length)
+{
+    switch (length) {
+    case 1:
+        *p_codepoint = **p_s;
+
+        break;
+    case 2:
+        *p_codepoint = (**p_s & 0x1F) << 6;
+
+        UTF8_TRY_INCREMENT_STRING_POINTER(p_s);
+        *p_codepoint |= **p_s & 0x3F;
+
+        break;
+    case 3:
+        *p_codepoint = (**p_s & 0x0F) << 12;
+
+        UTF8_TRY_INCREMENT_STRING_POINTER(p_s);
+        *p_codepoint |= (**p_s & 0x3F) << 6;
+
+        UTF8_TRY_INCREMENT_STRING_POINTER(p_s);
+        *p_codepoint |= **p_s & 0x3F;
+
+        break;
+    case 4:
+        *p_codepoint = (**p_s & 0x07) << 18;
+
+        UTF8_TRY_INCREMENT_STRING_POINTER(p_s);
+        *p_codepoint |= (**p_s & 0x3F) << 12;
+
+        UTF8_TRY_INCREMENT_STRING_POINTER(p_s);
+        *p_codepoint |= (**p_s & 0x3F) << 6;
+
+        UTF8_TRY_INCREMENT_STRING_POINTER(p_s);
+        *p_codepoint |= **p_s & 0x3F;
+
+        break;
+    default:
+        return UTF_BAD_BYTE; // BAD_LEAD
+    }
+
+    return UTF_OK;
+}
+
 enum utf_error utf8_validate_next(const char8_t **p_s, uint32_t *p_codepoint)
 {
     if (**p_s == 0)
-        return UTF_TRUNCATED;
+        return UTF_TRUNCATED; // NOT ENOUGH ROOM
 
     uint32_t cp = 0;
     int len = utf8_seqlen(*p_s);
-    enum utf_error err = UTF_OK;
+    enum utf_error err = utf8_getseq(p_s, &cp, len);
 
-    switch (len) {
-    case 0:
-        return UTF_BAD_BYTE;
-    case 1:
-        err = utf8_getseq1(p_s, &cp);
-        break;
-    case 2:
-        err = utf8_getseq2(p_s, &cp);
-        break;
-    case 3:
-        err = utf8_getseq3(p_s, &cp);
-        break;
-    case 4:
-        err = utf8_getseq4(p_s, &cp);
-        break;
-    }
-
-    if (err) return err;
+    if (err != UTF_OK)
+        return err;
 
     if (utf8_is_overlong(cp, len))
         return UTF_OVERLONG;
