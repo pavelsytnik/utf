2704ec3b8b34c9aca300f2219dd29e26252f6920
Pavel Sytník <pashasytnik@gmail.com>
2024-08-23 23:40:23 +0400

    Add utf_char8to32() for utf_str8get()

--- utf_strget.c
+++ utf_strget.c
@@ -14,6 +14,34 @@ static int utf_u8seqlen(char c)
         return 0;
 }
 
+// Maybe temporary, maybe not
+static uint32_t utf_char8to32(uint32_t u8c)
+{
+    if ((u8c & 0x80) == 0x00) {
+        return u8c;
+    } else if ((u8c >> 8 & 0xE0) == 0xC0) {
+        uint32_t out = 0;
+        out |= (u8c >> 8 & 0x1F) << 6;
+        out |= u8c & 0x3F;
+        return out;
+    } else if ((u8c >> 16 & 0xF0) == 0xE0) {
+        uint32_t out = 0;
+        out |= (u8c >> 16 & 0x0F) << 12;
+        out |= (u8c >> 8  & 0x3F) << 6;
+        out |= u8c & 0x3F;
+        return out;
+    } else if ((u8c >> 24 & 0xF8) == 0xF0) {
+        uint32_t out = 0;
+        out |= (u8c >> 24 & 0x07) << 18;
+        out |= (u8c >> 16 & 0x3F) << 12;
+        out |= (u8c >> 8  & 0x3F) << 6;
+        out |= u8c & 0x3F;
+        return out;
+    } else {
+        return 0xFFFFFFFF;
+    }
+}
+
 // Also unsafe code
 char32_t utf_str8get(const char8_t *s, size_t i)
 {
@@ -26,7 +54,7 @@ char32_t utf_str8get(const char8_t *s, size_t i)
     for (int i = 0; i < len; i++)
         ch = ch << 8 | *(s + i) & 0xFF;
 
-    return u8tou32(ch); // I forgot to change this line
+    return utf_char8to32(ch);
 }
 
 // TODO: append check for null-terminated character
