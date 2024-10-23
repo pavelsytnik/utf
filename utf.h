774ae1c9318a7d77abd987ac65c89b2140a18ff3
Pavel Sytník <civoghelo@gmail.com>
2024-10-23 18:34:04 +0400

    Refactor utf_strget.c and include missing headers in utf.h

--- utf.h
+++ utf.h
@@ -1,10 +1,12 @@
 #ifndef utf_h_
 #define utf_h_
 
+#include "utf_bool.h"
 #include "utf_byteorder.h"
 #include "utf_char.h"
 #include "utf_error.h"
 #include "utf_fio.h"
+#include "utf_lang.h"
 #include "utf_strconv.h"
 #include "utf_strget.h"
 #include "utf_strlen.h"
--- utf_strget.c
+++ utf_strget.c
@@ -1,53 +1,17 @@
 #include "utf_strget.h"
 
-// Maybe temporary, maybe not
-static utf_c32 utf_ch_8_to_32_(utf_c32 u8c)
-{
-    if ((u8c & 0x80) == 0x00) {
-        return u8c;
-    } else if ((u8c >> 8 & 0xE0) == 0xC0) {
-        utf_c32 out = 0;
-        out |= (u8c >> 8 & 0x1F) << 6;
-        out |= u8c & 0x3F;
-        return out;
-    } else if ((u8c >> 16 & 0xF0) == 0xE0) {
-        utf_c32 out = 0;
-        out |= (u8c >> 16 & 0x0F) << 12;
-        out |= (u8c >> 8  & 0x3F) << 6;
-        out |= u8c & 0x3F;
-        return out;
-    } else if ((u8c >> 24 & 0xF8) == 0xF0) {
-        utf_c32 out = 0;
-        out |= (u8c >> 24 & 0x07) << 18;
-        out |= (u8c >> 16 & 0x3F) << 12;
-        out |= (u8c >> 8  & 0x3F) << 6;
-        out |= u8c & 0x3F;
-        return out;
-    } else {
-        return 0xFFFFFFFF;
-    }
-}
+#include "utf_chrconv.h"
 
-// Also unsafe code
 utf_c32 utf_8_strget(const utf_c8 *s, size_t i)
 {
-    utf_c32 ch = 0;
-
-    while (i-- > 0)
-        s += utf_8_length_from_lead(*s);
-
-    int len = utf_8_length_from_lead(*s);
-    for (int i = 0; i < len; i++)
-        ch = ch << 8 | *(s + i) & 0xFF;
-
-    return utf_ch_8_to_32_(ch);
+    utf_c32 cp;
+    utf_8_chr_to_32_(utf_8_strat(s, i), &cp);
+    return cp;
 }
 
-// TODO: append check for null-terminated character
 utf_c8 *utf_8_strat(const utf_c8 *s, size_t i)
 {
     while (i-- > 0)
         s += utf_8_length_from_lead(*s);
-
     return s;
 }
